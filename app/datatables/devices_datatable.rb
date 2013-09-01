74ab3c7c79441f375ac68cf8d7921047db002a89
mixtli <ronmcclain75@gmail.com>
2013-09-01 02:40:12 -0700

    fix devices index view

--- app/datatables/devices_datatable.rb
+++ app/datatables/devices_datatable.rb
@@ -1,8 +1,8 @@
 class DevicesDatatable < Datatable
   self.model = Device
-  self.columns = [:hostname, :sys_name, :sys_descr]
+  self.columns = [:hostname, :state]
   self.default_scope = Device
   def row(device)
-    [link_to(device.hostname, device), device.sys_name, device.sys_descr]
+    [link_to(device.hostname, device), device.state]
   end
 end
